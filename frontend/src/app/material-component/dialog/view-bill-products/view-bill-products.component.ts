145352446a6bd7f2f0c05d5f26c28cd6db42f100
Abhishek t <abhishekthakare29@gmail.com>
2023-03-24 15:35:49 +0530

    Added Frontend and Backeng

---
+++ frontend/src/app/material-component/dialog/view-bill-products/view-bill-products.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Inject, EventEmitter } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-view-bill-products',
+  templateUrl: './view-bill-products.component.html',
+  styleUrls: ['./view-bill-products.component.scss']
+})
+export class ViewBillProductsComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  
+  }
+
+  
+}
